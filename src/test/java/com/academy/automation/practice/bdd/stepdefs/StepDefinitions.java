8f44f1951d52579be0ae7176dffbc9c96a26c373
Oleg <oleg.kh81@gmail.com>
2018-06-14 03:32:27 +0300

    bdd 'testLogin' demo

---
+++ src/test/java/com/academy/automation/practice/bdd/stepdefs/StepDefinitions.java
@@ -0,0 +1,79 @@
+package com.academy.automation.practice.bdd.stepdefs;
+
+import com.academy.automation.practice.manager.TestManager;
+import com.academy.automation.practice.page.HomePage;
+import com.academy.automation.practice.page.LoginPage;
+import com.academy.automation.practice.page.MyAccountPage;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.io.IOException;
+
+public class StepDefinitions {
+
+    private TestManager manager;
+
+    @Before
+    public void setUp() throws IOException {
+        manager = new TestManager();
+        manager.init("chrome");
+    }
+
+    @Given("^I am on home page$")
+    public void goToHomePage() {
+        manager.goTo().home();
+    }
+
+    @Then("^I click on signIn link$")
+    public void clickSignInLink() {
+        new HomePage(manager.getDriver())
+                .clickSignInLink();
+    }
+
+    @When("^I fill login '(.*?)'$")
+    public void fillLogin(String login) {
+        // Write code here that turns the phrase above into concrete actions
+        new LoginPage(manager.getDriver())
+                .inputLogin(login);
+    }
+
+    @And("^I fill password '(.*?)'$")
+    public void fillPassword(String password) {
+        new LoginPage(manager.getDriver())
+                .inputPassword(password);
+    }
+
+    @And("^click signIn button$")
+    public void i_click_on_the_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        new LoginPage(manager.getDriver())
+                .clickSignIn(true);
+    }
+
+    @Then("^I should see '(.*?)' link$")
+    public void i_should_see_the_button(String userName) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        Assert.assertEquals(
+                new MyAccountPage(manager.getDriver()).getUserNameCapture(),
+                userName);
+    }
+
+    @Then("^I should make logout$")
+    public void i_should_see_the_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        new MyAccountPage(manager.getDriver())
+                .logout();
+    }
+
+    @After
+    public void setDown(){
+        manager.stop();
+    }
+}
